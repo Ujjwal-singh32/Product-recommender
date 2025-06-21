a3fe1d5af9351198bbddaa5b640e604a51442ff2
Ujjwal-Singh32 <ujjwalchauhan654@gmail.com>
2025-06-21 16:32:34 +0530

    added some ports

--- app.py
+++ app.py
@@ -135,5 +135,5 @@ def search_green_products():
 
 if __name__ == "__main__":
 
-    port = int(os.environ.get("PORT", 5000))  # Use Render-assigned port if available
+    port = int(os.environ.get("PORT", 7860)) 
     app.run(host="0.0.0.0", port=port, debug=True)
